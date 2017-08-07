6118cd80c980e011d7f43f2078b47f791881f66e
wangzhen369 <369073048@qq.com>
2017-08-07 15:31:20 +0800

    活动管理部分代码提交

--- score-web/src/main/java/org/trc/resource/luckydraw/ActivityPrizesResource.java
+++ score-web/src/main/java/org/trc/resource/luckydraw/ActivityPrizesResource.java
@@ -1,24 +1,17 @@
 package org.trc.resource.luckydraw;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.trc.mall.util.CustomAck;
-import com.txframework.core.jdbc.PageRequest;
-import com.txframework.util.ListUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.trc.biz.luckydraw.IActivityPrizesBiz;
 import org.trc.constants.ScoreAdminConstants;
 import org.trc.domain.luckydraw.ActivityPrizesDO;
-import org.trc.domain.luckydraw.LuckyDrawDO;
-import org.trc.service.luckydraw.IActivityPrizesService;
+
 import org.trc.util.Pagenation;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
 
 /**
  * author: hzwzhen
@@ -30,7 +23,6 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Path(ScoreAdminConstants.Route.ActivityPrizes.ROOT)
 public class ActivityPrizesResource {
-
 
     private Logger logger = LoggerFactory.getLogger(ActivityPrizesResource.class);
 
